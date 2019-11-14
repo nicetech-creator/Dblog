882da321a8fccccd67d00d8867b4a3a6655aaf92
Top Tech <56525446+nicetech-creator@users.noreply.github.com>
2019-11-14 23:02:01 +0800

    taggit module integration

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
 #custome object manager
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -23,6 +25,7 @@ class Post(models.Model):
     
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
