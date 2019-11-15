86bd932fe07f45341c3c16e6b3d0adc0bdd6f69c
Top Tech <56525446+nicetech-creator@users.noreply.github.com>
2019-11-16 02:09:43 +0800

    full text search using postgreSQL

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 def post_share(request, post_id):
     # Retrieve post by id
@@ -87,3 +87,21 @@ def post_detail(request, year, month, day, post):
                 'similar_posts': similar_posts
                 })
 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(
+                rank=SearchRank(search_vector, search_query)
+                ).filter(rank__gte=0.3).order_by('-rank')
+    return render(request,
+    'blog/post/search.html',
+    {'form': form,
+    'query': query,
+    'results': results})
